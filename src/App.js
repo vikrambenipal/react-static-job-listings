7b35f81b625dd96e01cf18f345f0a0f93b0553e0
vikrambenipal <vikrambenipal@gmail.com>
2021-02-25 13:09:30 -0500

    working on flex layout for job posting using styled components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <List data={data}/>
-      
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styled from'styled-components';
 
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    `
 const Item = ({ item }) => {
     return (
         <div>
-            <h3>{item.company}</h3>
-            {item.new && 
-                <p>NEW!</p>
-            }
-            {item.featured && 
-                <p>FEATURED</p>
-            }
+            {/* <img src={require(item.logo)} alt=""></img> */}
+            <Row>
+                <h3>{item.company}</h3>
+                {item.new && 
+                    <h3>NEW!</h3>
+                }
+                {item.featured && 
+                    <h3>FEATURED</h3>
+                }
+            </Row>
             <h2>{item.position}</h2>
+            <Row>
+                <p>{item.postedAt}</p>
+                <p>{item.contract}</p>
+                <p>{item.location}</p>
+            </Row>
+            
             <hr></hr>
+            {/* Filter */}
             <ul>
                 <li>{item.role}</li>
                 <li>{item.level}</li>
